f6b70d99c2fe521d1c7ed8738d23f142b566ec3e
umgonji <umgonji@naver.com>
2020-05-24 16:10:42 +0900

    1000을 대신할 변수로 REQ_FOR_NICKNAME  - 코드의 가독성을 향상

--- app/src/main/java/dasdsa/sdn/androidbasic01_intent_20200524/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/androidbasic01_intent_20200524/MainActivity.kt
@@ -8,6 +8,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val REQ_FOR_NICKNAME = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -41,7 +43,7 @@ class MainActivity : AppCompatActivity() {
         }
         editNickNameBtn.setOnClickListener {
             val myIntent = Intent(this, EditNickNameActivity::class.java)
-            startActivityForResult(myIntent, 1000)
+            startActivityForResult(myIntent, REQ_FOR_NICKNAME )
         }
 
     }
@@ -49,13 +51,12 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(requestCode == 1000) {
+        if(requestCode == REQ_FOR_NICKNAME) {
             if(resultCode == Activity.RESULT_OK) {
-
                 val newNickName = data?.getStringExtra("nick")
-
                 nickNameTxt.text = newNickName
             }
         }
+
     }
 }
