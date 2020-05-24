eeb2e6d514eaefd711504f5098ce3302f4d4b5b8
umgonji <umgonji@naver.com>
2020-05-24 13:52:50 +0900

    버튼을 누르면 첫 액티비티로 실제 이동

--- app/src/main/java/dasdsa/sdn/androidbasic01_intent_20200524/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/androidbasic01_intent_20200524/MainActivity.kt
@@ -1,12 +1,20 @@
 package dasdsa.sdn.androidbasic01_intent_20200524
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        goToFirstBtn.setOnClickListener {
+
+            val myIntent = Intent(this, FirstActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
