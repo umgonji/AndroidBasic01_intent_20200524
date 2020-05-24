f2fe6b1d6ae3717f59590fe3293c2c452d693f3a
umgonji <umgonji@naver.com>
2020-05-24 14:02:24 +0900

    돌아가기 누르면 FirstActivity 종료처리

--- app/src/main/java/dasdsa/sdn/androidbasic01_intent_20200524/FirstActivity.kt
+++ app/src/main/java/dasdsa/sdn/androidbasic01_intent_20200524/FirstActivity.kt
@@ -1,12 +1,23 @@
 package dasdsa.sdn.androidbasic01_intent_20200524
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first.*
 
 class FirstActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        goBackBtn.setOnClickListener {
+//            새로 MainActivity를 하나 더 만들어서 얹어줌.
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+//            FirstActivity를 종료.
+            finish()
+        }
     }
 }
