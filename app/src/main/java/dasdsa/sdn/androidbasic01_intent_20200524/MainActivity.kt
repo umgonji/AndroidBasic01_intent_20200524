2fffb82a2139b13b7c25d44212bf224290a19fb0
umgonji <umgonji@naver.com>
2020-05-24 13:34:07 +0900

    Initial commit

---
+++ app/src/main/java/dasdsa/sdn/androidbasic01_intent_20200524/MainActivity.kt
@@ -0,0 +1,12 @@
+package dasdsa.sdn.androidbasic01_intent_20200524
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
